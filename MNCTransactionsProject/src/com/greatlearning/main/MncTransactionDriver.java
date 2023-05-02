ddc7371f732a336cb0893470b890ea13f5bc1e6a
arunayesaswini <127669982+arunayesaswini@users.noreply.github.com>
2023-05-02 11:08:09 +0530

    Add files via upload

---
+++ MNCTransactionsProject/src/com/greatlearning/main/MncTransactionDriver.java
@@ -0,0 +1,37 @@
+package com.greatlearning.main;
+
+import java.util.Scanner;
+
+import com.greatlearning.services.CompleteBinarySearchTree;
+
+public class MncTransactionDriver {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc=new Scanner(System.in);
+		CompleteBinarySearchTree cbst=new CompleteBinarySearchTree();
+		System.out.println("No. of Transactions to Enter ");
+		int n=sc.nextInt();
+		System.out.println("Enter Transactions");
+		for(int i=1;i<=n;i++)
+		{
+			double transaction=sc.nextDouble();
+			cbst.insert(transaction);
+		}
+		
+		try {
+		
+		System.out.println("Transactions entered are:");
+		cbst.convertTree();
+		}
+		catch(ArrayIndexOutOfBoundsException ae)
+		{
+			System.out.println("program is going beyond the limit: "+ae);
+		}
+		sc.close();
+		
+
+	}
+
+}
